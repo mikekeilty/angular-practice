f6af81df9818e4641f643d5fccf122ff11383c8c
Mike Keilty <mikekeilty@gmail.com>
2014-06-20 11:03:47 -0400

    set up module declaration

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,30 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
-  }]);
+angular.module('F1FeederApp.controllers', [])
+  .controller('driversController', function($scope) {
+    $scope.driversList = [
+        {
+            Driver: {
+                givenName: 'Sebastian',
+                familyName: 'Vettel'
+            },
+            points: 322,
+            nationality: 'German',
+            Constructors: [
+                { name: "Red Bull" }
+            ]
+        },
+        {
+            Driver: {
+                givenName: 'Fernando',
+                familyName: 'Alonzo'
+            },
+            points: 207,
+            nationality: 'Spanish',
+            Constructors: [
+                { name: "Ferrari" }
+            ]
+        }
+    ];
+  });
